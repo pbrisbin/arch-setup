3fc893e241cc4093ced1b03cac330d830c7219a0
patrick brisbin <pbrisbin@gmail.com>
2020-08-24 20:18:01 -0400

    Update acpi-handler.sh

--- files/acpi-handler.sh
+++ files/acpi-handler.sh
@@ -1,42 +1,46 @@
 #!/bin/sh
 
-amixer_() {
-  amixer -q -c 0 sset Headphone unmute
-  amixer -q -c 0 sset Speaker unmute
-  amixer -q "$@"
+runx() {
+  /home/patrick/.local/bin/runx "$@"
 }
 
 notify() {
-  /home/patrick/.local/bin/runx notify-send "$@"
+  runx dunstify "$@"
 }
 
 logger -t acpid "$*"
 
 case "$1" in
-  battery) notify "Battery Event" "$(acpi)" ;;
+  battery) notify -r 1 "Battery Event" "$(acpi)" ;;
   button/f20)
-    amixer -q -c 0 sset Capture toggle
-    amixer -q -c 1 sset Capture toggle
+    runx pactl set-source-mute @DEFAULT_SOURCE@ toggle
     ;;
   button/mute)
-    amixer_ -c 0 sset Master toggle
-    amixer_ -c 1 sset PCM toggle
+    runx pactl set-sink-mute @DEFAULT_SINK@ toggle
+    notify -r 2 "Volume" "$(runx /home/patrick/.local/bin/pactl-status)"
     ;;
   button/volumedown)
-    amixer_ -c 0 sset Master '3%-'
-    amixer_ -c 1 sset PCM '3%-'
+    runx pactl set-sink-mute @DEFAULT_SINK@ false
+    runx pactl set-sink-volume @DEFAULT_SINK@ "-7%"
+    notify -r 2 "Volume" "$(runx /home/patrick/.local/bin/pactl-status)"
     ;;
   button/volumeup)
-    amixer_ -c 0 sset Master '3%+'
-    amixer_ -c 1 sset PCM '3%+'
+    runx pactl set-sink-mute @DEFAULT_SINK@ false
+    runx pactl set-sink-volume @DEFAULT_SINK@ "+7%"
+    notify -r 2 "Volume" "$(runx /home/patrick/.local/bin/pactl-status)"
     ;;
   video/brightnessup)
     read -r val < /sys/class/backlight/intel_backlight/brightness
     echo $((val+77)) > /sys/class/backlight/intel_backlight/brightness
+    notify -r 3 "Brightness" "$(cat /sys/class/backlight/intel_backlight/brightness)"
     ;;
   video/brightnessdown)
     read -r val < /sys/class/backlight/intel_backlight/brightness
     echo $((val-77)) > /sys/class/backlight/intel_backlight/brightness
+    notify -r 3 "Brightness" "$(cat /sys/class/backlight/intel_backlight/brightness)"
+    ;;
+  button/wlan)
+    notify -r 4 "WLAN" "$(rfkill list wifi)"
     ;;
   #*) notify "ACPI Event" "$(printf "%s\n" "$@")" ;;
 esac
