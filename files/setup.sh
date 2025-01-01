c94ad9a9f51a98a794e5a103ebef958cd0bb0fbd
patrick brisbin <pbrisbin@gmail.com>
2025-01-01 15:28:37 -0500

    Fixup setup.sh

--- files/setup.sh
+++ files/setup.sh
@@ -4,14 +4,16 @@ set -e
 wl=$(ip link | sed '/^[0-9]*: \(wl[^:]*\):.*$/!d; s//\1/' | head -n 1)
 
 echo "Configuring network ($wl)"
-sudo systemctl enable "netctl-auto@$wl"
-sudo systemctl start "netctl-auto@$wl"
+sudo systemctl start "netctl-auto@$wl" systemd-resolved
+sudo systemctl enable "netctl-auto@$wl" systemd-resolved
 
-git clone https://github.com/pbrisbin/dotfiles ~/.dotfiles
+echo "Cloning dotfiles"
+dotfiles=$HOME/.dotfiles
+git clone https://github.com/pbrisbin/dotfiles "$dotfiles"
 
-~/.local/bin/aurtoi setup
+echo "Installing AUR packages"
+AUR_INVENTORY=$dotfiles/local/share/aur-packages.csv "$dotfiles"/local/bin/aurtoi setup
+AUR_INVENTORY=$dotfiles/local/share/aur-packages.csv "$dotfiles"/local/bin/aurtoi install
 
-aurto add rcm
-sudo pacman -Sy --noconfirm rcm
-
-RCRC=$HOME/.dotfiles/config/rcm/rcrc rcup -f
+echo "Installing dotfiles"
+RCRC=$dotfiles/config/rcm/rcrc rcup -f
